91779fe63a135813da7c1d0feb00d17a01d1dc1c
Mateusz <gorzanski.mateusz@gmail.com>
2019-01-17 00:14:42 +0100

    add navs to sidebar

--- src/containers/Calendar/Sidebar.js
+++ src/containers/Calendar/Sidebar.js
@@ -9,6 +9,17 @@ export default class Sidebar extends React.Component {
           <div className="show-hide-panel-button">
             <i className="fas fa-chevron-left"></i>
           </div>
+
+          <ul className="top-menu-buttons">
+            <li className="main-sidebar-menu-item-active"><i className="fas fa-calendar"></i></li>
+            <li><i className="far fa-chart-bar"></i></li>
+            <li><i className="fas fa-users"></i></li>
+            <li><i className="fas fa-address-card"></i></li>
+          </ul>
+
+          <ul className="bottom-menu-buttons">
+            <li><i className="fas fa-cog"></i></li>
+          </ul>
         </nav>
       </div>
     );
