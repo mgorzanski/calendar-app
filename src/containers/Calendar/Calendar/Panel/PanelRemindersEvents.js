2964034bc15102c999d56ca803296c231a508047
Mateusz <gorzanski.mateusz@gmail.com>
2019-04-13 16:02:07 +0200

    add Today tab to PanelRemindersEvents component

--- src/containers/Calendar/Calendar/Panel/PanelRemindersEvents.js
+++ src/containers/Calendar/Calendar/Panel/PanelRemindersEvents.js
@@ -49,6 +49,46 @@ export default class PanelRemindersEvents extends React.Component {
       </ul>
     );
 
+    const TodayEvents = () => (
+      <ul className="today-events-list">
+        <li className="today-events-list-item today-events-list-item-color-red"
+        onMouseMove={e => {
+            const x = e.pageX - e.target.offsetLeft;
+            const y = e.pageY - e.target.offsetTop;
+
+            e.target.style.setProperty('--x', `${x}px`);
+            e.target.style.setProperty('--y', `${y}px`);
+          }}
+        >
+          <div className="today-events-list-item-timestamp">
+            <span className="today-events-list-item-timestamp-time">11:30 AM</span>
+          </div>
+          <div className="today-events-list-item-details">
+            <span className="today-events-list-item-details-title">Finish homework</span>
+            <span className="today-events-list-item-details-calendar-name text-color-red">School</span>
+          </div>
+        </li>
+
+        <li className="today-events-list-item today-events-list-item-color-red"
+        onMouseMove={e => {
+            const x = e.pageX - e.target.offsetLeft;
+            const y = e.pageY - e.target.offsetTop;
+
+            e.target.style.setProperty('--x', `${x}px`);
+            e.target.style.setProperty('--y', `${y}px`);
+          }}
+        >
+          <div className="today-events-list-item-timestamp">
+            <span className="today-events-list-item-timestamp-time">11:30 AM</span>
+          </div>
+          <div className="today-events-list-item-details">
+            <span className="today-events-list-item-details-title">Finish homework</span>
+            <span className="today-events-list-item-details-calendar-name text-color-red">School</span>
+          </div>
+        </li>
+      </ul>
+    );
+
     return (
       <div className="panel-reminders-events">
         <Panel
@@ -66,7 +106,7 @@ export default class PanelRemindersEvents extends React.Component {
               name: 'Today',
               actionIcon: 'fas fa-plus',
               onActionIconPress: () => console.log('Press'),
-              content: <Reminders />,
+              content: <TodayEvents />,
               order: 1
             }
           ]}
